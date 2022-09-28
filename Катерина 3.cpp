a38fbe0d8d49301ded9866d1a5451dc2eb4ad16e
Lapikova09 <96071466+Lapikova09@users.noreply.github.com>
2022-09-28 18:26:15 +0300

    Add files via upload

---
+++ Катерина 3.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	//int a, b;
+	//cin >> a >> b;
+	//int c = 0;
+	//if (a > b) { c = a; a = b; b = c; }
+	//cout << a << endl; cout << b << endl;
+
+	//int a, b;
+	//cin >> a >> b;
+	//if (a == b) { a = 0; b = 0; }
+	//else { a = a + b; b = a; }
+	//cout << a << endl; cout << b << endl;
+	//system("pause");
+	//int a, b;	//cin >> a >> b;	//if (a == b) { a = 0; b = 0; }	//else if (a > b) { b = a; }
+	//else if (b > a) { a = b; }
+	//cout << a << endl; cout << b << endl;
+	//system("pause");
+
+	//int a, b, c;
+	//cin >> a >> b >> c;
+	//if ((a < b) && (b < c)) { cout << a << endl; }
+	//else if ((a < c) && (c < b)) { cout << a << endl; }
+	//else if ((b < c) && (c < a)) { cout << b << endl; }
+	//else if ((b <a) && (a < c)) { cout << b << endl; }
+	//elseif ((c < a) && (a < b)) { cout << c << endl; }
+	//else if ((c < b) && (b < a)) { cout << c << endl; }
+	//system("pause");
+
+	//int a, b, c;
+	//cin >> a >> b >> c;
+	//if ((a < b) && (b < c)) { cout << b << endl; }
+	//else if ((a < c) && (c < b)) { cout << c << endl; }
+	//else if ((b < c) && (c < a)) { cout << c << endl; }
+	//else if ((b <a) && (a < c)) { cout << a << endl; }
+	//else if ((c < a) && (a < b)) { cout << a << endl; }
+	//else if ((c < b) && (b < a)) { cout << b << endl; }
+	//system("pause");
+
+	//int a, b, c;
+	//cin >> a >> b >> c;
+	//if ((a < b) && (b < c)) { cout << a << endl; cout << c << endl;}
+	//else if ((a < c) && (c < b)) { cout << a << endl; cout << b << endl;}
+	//else if ((b < c) && (c < a)) { cout << b << endl; cout << a << endl;}
+	//else if ((b <a) && (a < c)) { cout << b << endl; cout << c << endl;}
+	//else if ((c < a) && (a < b)) { cout << c << endl; cout << b << endl;}
+	//else if ((c < b) && (b < a)) { cout << c << endl; cout << a << endl;}
+	//system("pause");
+
+	//int a, b, c, d;
+	//cin >> a >> b >> c;
+	//if ((a < b) && (b < c)) { d = b + c; cout << d << endl; }
+	//else if ((a < c) && (c < b)) { d = c = b; cout << d << endl; }
+	//elseif ((b < c) && (c < a)) { d = c + a; cout << d << endl; }
+	//else if ((b <a) && (a < c)) { d=a+c; cout << d << endl;}
+	//elseif ((c < a) && (a < b)) { d = a + b; cout << d << endl; }
+	//else if ((c < b) && (b < a)) { d = b + a; cout << d << endl; }
+    //system("pause");
+
+
+    //int a, b, c;
+	//cin >> a >> b >> c;
+	//if ((a < b) && (b < c)) { a = 2 * a;
+	//b = 2 * b;
+	//c = 2 * c; }
+	//else { a = a*-1; b = b*-1; c = c*-1; }
+	//cout << a << b << c << endl;
+	//system("pause");
+
+	//int a, b, c;
+	//cin >> a >> b >> c;
+	//if (((a < b) && (b < c)) || ((c < b) && (b < a))) { a = 2 * a;
+	//	b = 2 * b;
+	//	c = 2 * c; }
+	//	else { a = a*-1; b = b*-1; c = c*-1; }
+	//	cout << a << b << c << endl;
+	//	system("pause");
+
+	//int a, b, c;
+	//cin >> a >> b >> c;
+	//if (a == b) { cout << "3" << endl; }
+	//else if (a == c) { cout << "2" << endl; }
+	//else if (c == b) { cout << "1" << endl; }
+	//system("pause");
+
+
+	//int a, b, c, d;
+	//cin >> a >> b >> c >> d;
+	//if(a==b && b==c){ cout << "4" << endl; }
+	//else if (d == b && b == c) { cout << "1" << endl; }
+	//else if (a == d && d == c) { cout << "2" << endl; }
+	//else if (a == b && b == d) { cout << "3" << endl; }
+	//system("pause");
+
+
+	//int a, b, c, f, g;
+	//cin >> a >> b >> c;
+	//f = abs(b - a);
+	//g = abs(c - a);
+	//if (f > g) { cout << b << endl; cout << f << endl;
+	//}
+	//else if (g > f) {
+	//	cout << c << endl; cout << g << endl;
+	//}
+	//system("pause");
+
+
+}
